3d1f5ab899993b0fbbd041a2dbef661017e3a4df
hideOnDenis <denis.veliev@gmail.com>
2024-03-05 14:26:40 +0200

    orders adjusted

--- logistics-company-webapp/src/components/Orders.jsx
+++ logistics-company-webapp/src/components/Orders.jsx
@@ -1,85 +1,49 @@
-import Link from "@mui/material/Link";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchShipments } from "../features/shipments/shipmentSlice"; // Adjust the import path as necessary
+import {
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  Box,
+} from "@mui/material";
 import Title from "./Title";
-import * as React from "react";
-
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(
-    0,
-    "16 Mar, 2019",
-    "Elvis Presley",
-    "Tupelo, MS",
-
-    312.44
-  ),
-  createData(
-    1,
-    "16 Mar, 2019",
-    "Paul McCartney",
-    "London, UK",
-
-    866.99
-  ),
-  createData(
-    2,
-    "16 Mar, 2019",
-    "Tom Scholz",
-    "Boston, MA",
-
-    100.81
-  ),
-  createData(
-    3,
-    "16 Mar, 2019",
-    "Michael Jackson",
-    "Gary, IN",
-
-    654.39
-  ),
-  createData(
-    4,
-    "15 Mar, 2019",
-    "Bruce Springsteen",
-    "Long Branch, NJ",
-
-    212.79
-  ),
-];
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
 export default function Orders() {
+  const dispatch = useDispatch();
+  const { shipments, status } = useSelector((state) => state.shipments);
+
+  useEffect(() => {
+    dispatch(fetchShipments());
+  }, [dispatch]);
+
   return (
     <React.Fragment>
       <Title>Recent Orders</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell>Sender</TableCell>
+            <TableCell>Destination</TableCell>
+            <TableCell>Status</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell align="right">{`$50`}</TableCell>
+          {shipments.map((shipment) => (
+            <TableRow key={shipment._id}>
+              <TableCell>{shipment._id}</TableCell>
+              <TableCell>{shipment.createdBy.email}</TableCell>
+              <TableCell>{shipment.destination}</TableCell>
+              <TableCell>{shipment.status}</TableCell>
             </TableRow>
           ))}
         </TableBody>
